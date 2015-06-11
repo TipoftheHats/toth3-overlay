fe4c3fe61e783f9d540353f8e41c8e33ff2c86dc
Alex Van Camp <email@alexvan.camp>
2015-06-11 16:48:14 -0500

    Scoreboard has pretty decent controls now

--- dashboard/donations.js
+++ dashboard/donations.js
@@ -1,13 +1,15 @@
-'use strict';
+(function() {
+    'use strict';
 
-var $panel = $bundle.filter('.donations');
-var $test = $panel.find('button[command="test"]');
-var $amount = $panel.find('input[field="amount"]');
-var $donor = $panel.find('input[field="donor"]');
+    var $panel = $bundle.filter('.donations');
+    var $test = $panel.find('button[command="test"]');
+    var $amount = $panel.find('input[field="amount"]');
+    var $donor = $panel.find('input[field="donor"]');
 
-$test.click(function () {
-    nodecg.sendMessage('donation', {
-        amount: $amount.val(),
-        donor__visiblename: $donor.val()
+    $test.click(function () {
+        nodecg.sendMessage('donation', {
+            amount: $amount.val(),
+            donor__visiblename: $donor.val()
+        });
     });
-});
+})();
--- dashboard/lowerthird.js
+++ dashboard/lowerthird.js
@@ -1,80 +1,82 @@
-'use strict';
+(function() {
+    'use strict';
 
-var $panel = $bundle.filter('.lowerthird');
-var $take = $panel.find('button[command="take"]');
-var $show = $panel.find('button[command="show"]');
-var $hide = $panel.find('button[command="hide"]');
-var $pulse = $panel.find('button[command="pulse"]');
-var $preview = $panel.find('.js-preview');
-var $program = $panel.find('.js-program');
+    var $panel = $bundle.filter('.lowerthird');
+    var $take = $panel.find('button[command="take"]');
+    var $show = $panel.find('button[command="show"]');
+    var $hide = $panel.find('button[command="hide"]');
+    var $pulse = $panel.find('button[command="pulse"]');
+    var $preview = $panel.find('.js-preview');
+    var $program = $panel.find('.js-program');
 
-var texts = nodecg.Replicant('texts')
-    .on('change', function(oldVal, newVal) {
-        $program.find('.js-title').val(newVal.title);
-        $program.find('.js-body').val(newVal.body);
+    var texts = nodecg.Replicant('texts')
+        .on('change', function(oldVal, newVal) {
+            $program.find('.js-title').val(newVal.title);
+            $program.find('.js-body').val(newVal.body);
 
-        var evt = document.createEvent('Event');
-        var el = $program.find('.js-body')[0];
-        evt.initEvent('autosize:update', true, false);
-        el.dispatchEvent(evt);
-        simulateClick(el);
+            var evt = document.createEvent('Event');
+            var el = $program.find('.js-body')[0];
+            evt.initEvent('autosize:update', true, false);
+            el.dispatchEvent(evt);
+            simulateClick(el);
+        });
+
+    var lowerthirdShowing = nodecg.Replicant('lowerthirdShowing')
+        .on('change', function(oldVal, newVal) {
+            $show.prop('disabled', newVal);
+            $pulse.prop('disabled', newVal);
+            $hide.prop('disabled', !newVal);
+
+            // When this changes, disable the "take" button for a bit
+            if ($take.data('cooldownTimer')) {
+                clearTimeout($take.data('cooldownTimer'));
+            }
+
+            $take.prop('disabled', true);
+            $take.data('cooldownTimer', setTimeout(function() {
+                $take.prop('disabled', false);
+            }, 1000));
+        });
+
+    nodecg.Replicant('lowerthirdPulsing')
+        .on('change', function(oldVal, newVal) {
+            $hide.prop('disabled', !lowerthirdShowing.value ? true : newVal);
+        });
+
+    $take.click(function () {
+        texts.value = {
+            title: $preview.find('.js-title').val(),
+            body: $preview.find('.js-body').val()
+        };
     });
 
-var lowerthirdShowing = nodecg.Replicant('lowerthirdShowing')
-    .on('change', function(oldVal, newVal) {
-        $show.prop('disabled', newVal);
-        $pulse.prop('disabled', newVal);
-        $hide.prop('disabled', !newVal);
-
-        // When this changes, disable the "take" button for a bit
-        if ($take.data('cooldownTimer')) {
-            clearTimeout($take.data('cooldownTimer'));
-        }
-
-        $take.prop('disabled', true);
-        $take.data('cooldownTimer', setTimeout(function() {
-            $take.prop('disabled', false);
-        }, 1000));
+    $show.click(function () {
+        lowerthirdShowing.value = true;
     });
 
-nodecg.Replicant('lowerthirdPulsing')
-    .on('change', function(oldVal, newVal) {
-        $hide.prop('disabled', !lowerthirdShowing.value ? true : newVal);
+    $hide.click(function () {
+        lowerthirdShowing.value = false;
     });
 
-$take.click(function () {
-    texts.value = {
-        title: $preview.find('.js-title').val(),
-        body: $preview.find('.js-body').val()
-    };
-});
+    $pulse.click(function () {
+        nodecg.sendMessage('pulse', $(this).data('duration'));
+    });
 
-$show.click(function () {
-    lowerthirdShowing.value = true;
-});
-
-$hide.click(function () {
-    lowerthirdShowing.value = false;
-});
-
-$pulse.click(function () {
-    nodecg.sendMessage('pulse', $(this).data('duration'));
-});
-
-/*
- * Make the "Body" textareas automatically size themselves
- */
-autosize($panel[0].querySelectorAll('textarea'));
-$preview.find('.js-body').on('autosize:resized', function(){
-    simulateClick(this);
-});
+    /*
+     * Make the "Body" textareas automatically size themselves
+     */
+    autosize($panel[0].querySelectorAll('textarea'));
+    $preview.find('.js-body').on('autosize:resized', function(){
+        simulateClick(this);
+    });
 
 // Simulates a "click" event, which triggers Masonry to re-position the panels if necessary.
-function simulateClick(el) {
-    var event = new MouseEvent('click', {
-        'view': window,
-        'bubbles': true,
-        'cancelable': true
-    });
-    el.dispatchEvent(event);
-}
+    function simulateClick(el) {
+        var event = new MouseEvent('click', {
+            'view': window,
+            'bubbles': true,
+            'cancelable': true
+        });
+        el.dispatchEvent(event);
+    }
+})();
