549211297685eaf1c4dc0f1a63597c5aec0b1bb8
Alex Van Camp <email@alexvan.camp>
2015-09-08 21:29:57 -0500

    [donations] Add hardcoded secret key

--- extension/donations.js
+++ extension/donations.js
@@ -1,8 +1,14 @@
 'use strict';
 
+// TODO: Make this pull from the config, and not be hardcoded
+var SECRET_KEY = 'VjAJRS95)57I8DL';
 var app = require('express')();
 module.exports = function(nodecg) {
     app.post('/toth3-overlay/donation', function(req, res){
+        if (req.query.key !== SECRET_KEY) {
+            res.status(403).send('Not Authorized');
+            return;
+        }
         nodecg.sendMessage('donation', req.body);
         res.end();
     });
