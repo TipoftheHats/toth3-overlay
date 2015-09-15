7b3b6960205ef7b29bacd669d55f2819694557ce
Alex Van Camp <email@alexvan.camp>
2015-09-15 12:17:41 -0500

    [brb] Add visibility controls

--- dashboard/brb.js
+++ dashboard/brb.js
@@ -3,6 +3,8 @@
 
     var $panel = $bundle.filter('.brb');
     var $take = $panel.find('button[command="take"]');
+    var $show = $panel.find('button[command="show"]');
+    var $hide = $panel.find('button[command="hide"]');
     var $preview = $panel.find('.js-preview');
     var $program = $panel.find('.js-program');
 
@@ -10,6 +12,20 @@
         .on('change', function(oldVal, newVal) {
             $program.find('.js-upNext').val(newVal);
         });
+
+    var brbShowing = nodecg.Replicant('brbShowing')
+        .on('change', function(oldVal, newVal) {
+            $show.prop('disabled', newVal);
+            $hide.prop('disabled', !newVal);
+        });
+
+    $show.click(function () {
+        brbShowing.value = true;
+    });
+
+    $hide.click(function () {
+        brbShowing.value = false;
+    });
 
     $take.click(function () {
         upNext.value = $preview.find('.js-upNext').val();
